522f7786fab07470ef550085378631b0ed7c4fe2
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-03-08 15:22:42 +0200

    Add project files.

---
+++ directoryFilesSearch/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace directoryFilesSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(324, 610);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 200);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1141, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 193);
+            this.button2.TabIndex = 2;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(368, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(783, 610);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 635);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ directoryFilesSearch/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace directoryFilesSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class ImageGallery
+        {
+            public List<Image> images = new List<Image>();
+            public ImageGallery() { }
+            public ImageGallery(ImageList images) { }
+            public void AddImage(Image image) { images.Add(image); }
+            public void RemoveImage(Image image) { images.Remove(image); }
+            public Image GetImage(int index) { return images[index]; }
+            public int counter = 0;
+            public int current = 0;
+        }
+
+        public ImageGallery ig = new ImageGallery();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog1.ShowDialog();
+            this.Text = this.folderBrowserDialog1.SelectedPath.ToString();
+            DirectoryInfo dinf = new DirectoryInfo(this.folderBrowserDialog1.SelectedPath);
+            IEnumerable<FileInfo> infos;
+            List<string> allimagesingallery = new List<string>();
+
+            //infos.Append<dinf.EnumerateFiles("*.png")>;
+            
+            infos = dinf.EnumerateFiles("*.png");
+            this.textBox1.Text += "\r\n";
+
+
+            Image tmpimg;
+            
+
+
+            foreach (FileInfo info in infos)
+            {
+                this.textBox1.Text += info.Name;
+                this.textBox1.Text += "\r\n";
+                allimagesingallery.Add(info.Name);
+
+                tmpimg = Image.FromFile(this.Text+"\\"+info.Name);
+                imageList1.Images.Add(tmpimg);
+
+                ig.counter++;
+            }
+
+            infos = dinf.EnumerateFiles("*.jpg");
+            this.textBox1.Text += "\r\n";
+            foreach (FileInfo info in infos)
+            {
+                this.textBox1.Text += info.Name;
+                this.textBox1.Text += "\r\n";
+                allimagesingallery.Add(info.Name);
+
+                tmpimg = Image.FromFile(this.Text + "\\" + info.Name);
+                imageList1.Images.Add(tmpimg);
+
+                ig.counter++;
+            }
+
+            infos = dinf.EnumerateFiles("*.jpeg");
+            this.textBox1.Text += "\r\n";
+            foreach (FileInfo info in infos)
+            {
+                this.textBox1.Text += info.Name;
+                this.textBox1.Text += "\r\n";
+                allimagesingallery.Add(info.Name);
+
+                tmpimg = Image.FromFile(this.Text + "\\" + info.Name);
+                imageList1.Images.Add(tmpimg);
+
+
+                ig.counter++;
+            }
+
+            infos = dinf.EnumerateFiles("*.bmp");
+            this.textBox1.Text += "\r\n";
+            foreach (FileInfo info in infos)
+            {
+                this.textBox1.Text += info.Name;
+                this.textBox1.Text += "\r\n";
+                allimagesingallery.Add(info.Name);
+
+                tmpimg = Image.FromFile(this.Text + "\\" + info.Name);
+                imageList1.Images.Add(tmpimg);
+
+
+                ig.counter++;
+            }
+
+            infos = dinf.EnumerateFiles("*.gif");
+            this.textBox1.Text += "\r\n";
+            foreach (FileInfo info in infos)
+            {
+                this.textBox1.Text += info.Name;
+                this.textBox1.Text += "\r\n";
+                allimagesingallery.Add(info.Name);
+
+                tmpimg = Image.FromFile(this.Text + "\\" + info.Name);
+                imageList1.Images.Add(tmpimg);
+
+
+                ig.counter++;
+            }
+
+
+
+            this.Text = Application.ExecutablePath;
+
+
+            for (int i = 0; i < ig.counter; i++)
+            {
+                ig.images.Add(imageList1.Images[i]);
+            }
+            
+
+            this.pictureBox1.BackgroundImage = ig.images[0];
+            ig.current = 0;
+
+            this.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ig.current == 0)
+            {
+                ig.current = ig.counter;
+            }
+            else
+            {
+                ig.current--;
+            }
+            this.pictureBox1.BackgroundImage = ig.images[ig.current];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ig.current == ig.counter)
+            {
+                ig.current = 0;
+            }
+            else
+            {
+                ig.current++;
+            }
+            this.pictureBox1.BackgroundImage = ig.images[ig.current];
+        }
+    }
+}
